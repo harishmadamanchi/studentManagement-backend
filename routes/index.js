72090b1f3807b50373d2b17e0d438a7a3e01e295
Harish M <harishmadamanchi95@gmail.com>
2021-11-04 16:40:05 +0530

    Added delete and update

--- routes/index.js
+++ routes/index.js
@@ -91,4 +91,86 @@ router.post("/add-multiple-students", async (req, res) => {
   }
 });
 
+router.put("/edit-student/:id", async (req, res) => {
+  //edit a student
+
+  const client = await mongoClient.connect(MONGODBURL);
+
+  try {
+    const db = client.db("studentManagement");
+    let data = await db.collection("students").updateOne(
+      { _id: ObjectId(req.params.id) },
+      {
+        $set: {
+          name: req.body.name,
+          email: req.body.email,
+          mobile: req.body.mobile,
+        },
+      }
+    );
+
+    res.send({
+      message: "Success",
+      details: data,
+    });
+  } catch (error) {
+    res.send({
+      message: "Error in connection " + error,
+    });
+  } finally {
+    client.close();
+  }
+});
+
+router.delete("/delete-student/:id", async (req, res) => {
+  //delete by id
+  const client = await mongoClient.connect(MONGODBURL);
+
+  try {
+    const db = client.db("studentManagement");
+    let data = await db
+      .collection("students")
+      .deleteOne({ _id: ObjectId(req.params.id) });
+    res.send({
+      message: "Success",
+      details: data,
+    });
+  } catch (error) {
+    res.send({
+      message: "Error in connection " + error,
+    });
+  } finally {
+    client.close();
+  }
+});
+
+router.delete("/delete-many", async (req, res) => {
+  // delete many
+
+  const client = await mongoClient.connect(MONGODBURL);
+
+  try {
+    const db = client.db("studentManagement");
+
+    let deleteIDS = [];
+    req.body.map((element) => {
+      deleteIDS.push(ObjectId(element.id));
+    });
+
+    let data = await db
+      .collection("students")
+      .deleteMany({ _id: { $in: deleteIDS } });
+    res.send({
+      message: "Success",
+      details: data,
+    });
+  } catch (error) {
+    res.send({
+      message: "Error in Connection " + error,
+    });
+  } finally {
+    client.close();
+  }
+});
+
 module.exports = router;
